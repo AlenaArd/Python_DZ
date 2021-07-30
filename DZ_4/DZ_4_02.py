db9bdcc838edb4f6ed902cf0e151bf31b271fcf9
Alena <ardyuk_w@yahoo.com>
2021-07-30 03:02:37 +0300

    DZ_04 preparation stage

---
+++ DZ_4/DZ_4_02.py
@@ -0,0 +1,3 @@
+in_list = [12, 13,-2, 0, 453, 234, 34, 300, 2, 62, 544, 7, 740, 2876, 40, 78]
+good_list = [in_list[i] for i in range(1, len(in_list)) if in_list[i] > in_list[i-1]]
+print(good_list)
---
+++ DZ_4/DZ_4_03.py
@@ -0,0 +1,2 @@
+good_numbers = [n for n in range(20, 241) if n % 20 == 0 or n % 21 == 0]
+print(good_numbers)
