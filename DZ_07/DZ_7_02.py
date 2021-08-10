17d5d8872c14820bba7a0f85bf0d3a9ea5bb18b9
Alena <ardyuk_w@yahoo.com>
2021-08-11 00:57:47 +0300

    Py_07

--- DZ_07/DZ_7_02.py
+++ DZ_07/DZ_7_02.py
@@ -2,8 +2,13 @@ from abc import ABC, abstractmethod
 
 
 class Clothes(ABC):
+<<<<<<< HEAD
     @abstractmethod
     def consumption(self):
+=======
+    @abcstractmethod
+    def consumplition(self):
+>>>>>>> Py_DZ_07
         pass
 
 
@@ -12,7 +17,11 @@ class Coat(Clothes):
         self.size = size
 
     @property
+<<<<<<< HEAD
     def consumption(self):
+=======
+    def consumplition(self):
+>>>>>>> Py_DZ_07
         return self.size / 6.5 + 0.5
 
 
@@ -20,6 +29,7 @@ class Suit(Clothes):
     def __init__(self, h):
         self.h = h
 
+<<<<<<< HEAD
     @property
     def consumption(self):
         return 2 * self.h + 0.3
@@ -27,5 +37,14 @@ class Suit(Clothes):
 
 coat = Coat(46)
 costume = Suit(1.6)
+=======
+    @prorerty
+    def consumplition(self):
+        return 2 * self.h + 0.3
+
+
+coat = Coat(50)
+costume = Suit(1.96)
+>>>>>>> Py_DZ_07
 print(coat.consumption)
 print(costume.consumption)
